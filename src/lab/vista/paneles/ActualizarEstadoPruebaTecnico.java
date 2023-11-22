5ba94d402c9630f1230076c2038df63e2cb63d5a
Nicolas Repollo <nicolasrepollo2000@hotmail.com>
2023-11-22 15:00:31 -0300

    Se agrega que solo se puede aceptar o rechazar prueba cuando esta en estado pendiente

--- src/lab/vista/paneles/ActualizarEstadoPruebaTecnico.java
+++ src/lab/vista/paneles/ActualizarEstadoPruebaTecnico.java
@@ -2,6 +2,7 @@ package lab.vista.paneles;
 
 import java.awt.GridLayout;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -22,67 +23,71 @@ import lab.modelo.enums.EstadoLote;
 import lab.vista.tablas.ModeloLaboratorio;
 import lab.vista.tablas.ModeloPruebaLote;
 import lab.vista.view.PruebaLoteView;
+import net.miginfocom.swing.MigLayout;
 
-public class ActualizarEstadoPruebaTecnico extends JPanel{
+public class ActualizarEstadoPruebaTecnico extends JPanel {
 	private ControladorTecnico controlador;
+
 	public ActualizarEstadoPruebaTecnico() throws SedeNoEncontrada, LaboratorioNoEncontrado {
 		controlador = ControladorTecnico.getInstance();
-		GridLayout layout = new GridLayout(4, 2);
+		MigLayout layout = new MigLayout("fill", "", "");
 		setLayout(layout);
-		
+
 		ModeloLaboratorio modeloLab = new ModeloLaboratorio(controlador.listarLaboratorios());
 		JTable tablaLabs = new JTable(modeloLab);
 		JScrollPane scrollPaneLabs = new JScrollPane(tablaLabs);
-		
-		
+
 		ModeloPruebaLote modeloPruebas = new ModeloPruebaLote(new ArrayList<PruebaLoteView>());
 		JTable tablaPruebas = new JTable(modeloPruebas);
 		JScrollPane scrollPane = new JScrollPane(tablaPruebas);
-		
+
 		JComboBox<EstadoLote> comboEstados = new JComboBox<>();
 		comboEstados.addItem(EstadoLote.RECHAZADO);
 		comboEstados.addItem(EstadoLote.ACEPTADO);
-	
+
 		JButton btnActualizarEstado = new JButton("Actualizar Estado Prueba");
-		add(new JLabel("Laboratorios:"));
-		add(scrollPaneLabs);
-		add(new JLabel("Pruebas:"));
-		add(scrollPane);
+		btnActualizarEstado.setEnabled(false);
+
+		add(new JLabel("Laboratorios:"), "top");
+		add(scrollPaneLabs, "wrap, grow");
+		add(new JLabel("Pruebas:"), "top");
+		add(scrollPane, "wrap, grow");
 		add(new JLabel("Estados:"));
-		add(comboEstados);
-		add(new JLabel(""));
-		add(btnActualizarEstado);
-		
-		tablaLabs.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-			
-			@Override
-			public void valueChanged(ListSelectionEvent e) {
-				int value = (int)tablaLabs.getValueAt(tablaLabs.getSelectedRow(), 0);
-				try {
-					modeloPruebas.setDatos(controlador.listarPruebasEnLaboratorio(value));
-					modeloPruebas.fireTableDataChanged();
-				} catch (LaboratorioNoEncontrado | SedeNoEncontrada e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
+		add(comboEstados, "wrap, grow");
+		add(btnActualizarEstado, "skip, right");
+
+		tablaLabs.getSelectionModel().addListSelectionListener((e) -> {
+			int value = (int) tablaLabs.getValueAt(tablaLabs.getSelectedRow(), 0);
+			try {
+				modeloPruebas.setDatos(controlador.listarPruebasEnLaboratorio(value));
+				modeloPruebas.fireTableDataChanged();
+				btnActualizarEstado.setEnabled(false);
+			} catch (LaboratorioNoEncontrado | SedeNoEncontrada e1) {
+				e1.printStackTrace();
 			}
 		});
+
+		tablaPruebas.getSelectionModel().addListSelectionListener((e) -> {
+			if (tablaPruebas.getSelectedRow() < 0)
+				return;
+			PruebaLoteView pruebaSeleccionada = modeloPruebas.getDatos().get(tablaPruebas.getSelectedRow());
+			btnActualizarEstado.setEnabled(EstadoLote.PENDIENTE.equals(pruebaSeleccionada.getEstadoLote()));
+
+		});
+
 		btnActualizarEstado.addActionListener(e -> {
-			int idLab = (int)tablaLabs.getValueAt(tablaLabs.getSelectedRow(), 0);
+			int idLab = (int) tablaLabs.getValueAt(tablaLabs.getSelectedRow(), 0);
 			int idLote = (int) tablaPruebas.getValueAt(tablaPruebas.getSelectedRow(), 0);
 			EstadoLote estado = comboEstados.getItemAt(comboEstados.getSelectedIndex());
 			try {
-				controlador.actualizarPrueba(idLab, idLote,estado, new CriterioMolecular(""));
+				controlador.actualizarPrueba(idLab, idLote, estado, new CriterioMolecular(""));
 				JOptionPane.showMessageDialog(null, "Creado con exito!");
+				controlador.mostrarActualizarEstadoPrueba();
 			} catch (SedeNoEncontrada | PruebaLoteNoEncontrado | LaboratorioNoEncontrado e1) {
-				// TODO Auto-generated catch block
 				JOptionPane.showMessageDialog(null, "Algo no salio como se esperaba...");
 				e1.printStackTrace();
 			}
 		});
-		
-		
-		
-		
+
 	}
 }
